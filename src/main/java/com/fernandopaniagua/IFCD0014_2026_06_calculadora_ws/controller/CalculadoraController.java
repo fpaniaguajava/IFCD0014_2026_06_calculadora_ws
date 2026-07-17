b8fa3d3d2d43af719db4f7a668429727cfb95577
Fernando Paniagua <fernando.paniagua.java@gmail.com>
2026-07-17 16:07:06 +0200

    Cambios

--- src/main/java/com/fernandopaniagua/IFCD0014_2026_06_calculadora_ws/controller/CalculadoraController.java
+++ src/main/java/com/fernandopaniagua/IFCD0014_2026_06_calculadora_ws/controller/CalculadoraController.java
@@ -2,6 +2,7 @@ package com.fernandopaniagua.IFCD0014_2026_06_calculadora_ws.controller;
 
 
 import com.fernandopaniagua.IFCD0014_2026_06_calculadora_ws.service.ICalculadoraService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculadoraController {
     private ICalculadoraService iCalculadoraService;
+
     public CalculadoraController(ICalculadoraService iCalculadoraService) {
         this.iCalculadoraService = iCalculadoraService;
     }
+
+    @Operation(summary = "Suma dos números enteros")
     @GetMapping("sumar")
     public int sumar(@RequestParam int s1, @RequestParam int s2) {
         return iCalculadoraService.suma(s1, s2);
